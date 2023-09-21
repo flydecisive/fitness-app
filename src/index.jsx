d84ee6284a79e56b3b1d5c75fb82f182fc11eb5f
flydecisive <flydecisive@yandex.ru>
2023-09-21 22:25:50 +0300

    Добавляет шрифты

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "./fonts/Stratos-Regular.woff2";
+import "./fonts/Stratos-Regular.woff";
 import "./index.module.css";
 import App from "./App";
 
