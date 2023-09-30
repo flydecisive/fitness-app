65bf7c93aecb846231ce09cfc9dd57feec3d318d
DVS-675 <dmitryvs20170@gmail.com>
2023-09-30 15:05:25 +0500

    commit

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,12 +1,13 @@
 import styles from "./input.module.css";
 
-function Input({ type, placeholder, onInput, id }) {
+function Input({ type, placeholder, onInput, onChange, id }) {
   return (
     <input
       type={type}
       className={styles.input}
       placeholder={placeholder}
       onInput={onInput}
+      onChange={onChange}
       id={id}
     />
   );
