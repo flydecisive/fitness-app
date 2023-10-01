edceac2d17910b6adf45822b412b6fdab580308e
flydecisive <flydecisive@yandex.ru>
2023-10-01 20:35:41 +0300

    реализует выход из профиля

--- src/components/user-item/user-item.jsx
+++ src/components/user-item/user-item.jsx
@@ -1,15 +1,23 @@
 import styles from "./user-item.module.css";
 import { NavLink } from "react-router-dom";
 import { ReactComponent as Exit } from "../../assets/img/exit.svg";
+import { useNavigate } from "react-router-dom";
 
 function UserItem({ userName }) {
+  const navigate = useNavigate();
   return (
     <div className={styles.wrapper}>
       <NavLink className="link" to="/profile">
         <p className={styles.user}>{userName}</p>
       </NavLink>
 
-      <button className={styles.button}>
+      <button
+        className={styles.button}
+        onClick={() => {
+          localStorage.clear();
+          navigate("/");
+        }}
+      >
         <Exit className={styles.svg} />
       </button>
     </div>
